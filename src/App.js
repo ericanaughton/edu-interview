c6ec164de07b0595bad27082957d5afcdc065d14
Erica Naughton <erica@portalmedia.com>
2017-10-02 12:18:41 -0500

    New component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NewThing from './components/NewThing.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Erica Miller</h1>
         </header>
         <p className="App-intro">
           Let's get ready to rumble!
         </p>
+        <NewThing />
       </div>
     );
   }
---
+++ src/components/NewThing.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NewThing extends Component {
+	render() {
+		return (
+			<div className="NewThing">
+				<header className="NewThing-header">
+					<h1 className="NewThing-title">New thing</h1>
+				</header>
+			</div>
+		);
+	}
+}
+
+export default NewThing;
