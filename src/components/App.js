661f58c7ba27abb8025f7945cb84773b1ad5c5ad
Erica Naughton <erica@portalmedia.com>
2017-10-02 20:36:39 -0500

    Styles header

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import '../App.css';
+import '../index.css';
 import NewThing from './NewThing.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="container  center">
+      <div>
         <div className="header">
-          <h1 className="title">Erica Miller</h1>
+          <div className="name">Erica Miller</div>
+          <div className="company">Eduphoria</div>
         </div>
 
-        <NewThing />
+        <div className="container">
+          <NewThing />
+        </div>
       </div>
     );
   }
